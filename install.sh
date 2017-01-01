37f8a15c51d7a5c3d239d87b7f4d30d49125b233
root <root@master.local>
2017-01-01 03:58:25 -0500

    hbase 1.2.4

--- install.sh
+++ install.sh
@@ -154,6 +154,16 @@ spark.executor.memory            512m
 EOF
 
 cd ~
+wget http://apache.rediris.es/hbase/1.2.4/hbase-1.2.4-src.tar.gz
+tar -xvf hbase-1.2.4-src.tar.gz 
+cd hbase-1.2.4
+mvn clean package assembly:single -DskipTests -Dhadoop.version=2.7.3 -Dzookeeper.version=3.4.8
+tar -C/opt -xvf hbase-assembly/target/hbase-1.2.4-bin.tar.gz
+mv /opt/hbase-* /opt/hbase
+echo "PATH=\"/opt/hbase/bin:\$PATH\"" >> ~/.bashrc
+source ~/.bashrc
+
+cd ~
 wget http://apache.rediris.es/hive/hive-2.1.1/apache-hive-2.1.1-src.tar.gz
 tar -xvf apache-hive-2.1.1-src.tar.gz
 cd apache-hive-2.1.1-src
